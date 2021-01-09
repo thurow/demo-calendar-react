7c4f14730557ca866a9e9333f36d7aee249bdca3
Alexandre Thurow <alexandre.fredericothurow@gmail.com>
2021-01-08 21:34:15 -0300

    setup webpack and babel

---
+++ webpack.config.ts
@@ -0,0 +1,53 @@
+import path from "path";
+import webpack from "webpack";
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+
+const config: webpack.Configuration = {
+    mode: "development",
+    entry: "./src/index.tsx",
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react",
+                            "@babel/preset-typescript",
+                        ],
+                    },
+                },
+            },
+        ],
+    },
+    devtool: 'inline-source-map',
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    target: 'web',
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "bundle.js",
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        inline: true,
+        open: true,
+        hot: true,
+        historyApiFallback: true,
+        port: 3005
+    },
+    plugins: [
+        new ForkTsCheckerWebpackPlugin({
+            async: false,
+            eslint: {
+                files: "./src/**/*",
+            },
+        }),
+    ],
+};
+
+export default config;
